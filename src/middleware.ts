7c9c99c761bb2bd36966fe088b337ba54892dbdc
Tushar Gupta <tusharguptagps@gmail.com>
2026-04-29 00:41:33 +0530

    Add Feature logs and login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/", "/api/health"],
+});
 
 export const config = {
   matcher: [
